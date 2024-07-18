5c8012cb8e6eeb94023f2ad37b50f676c8713621
Tatsuya Teshima <terima666tatsuya@gmail.com>
2024-07-18 18:18:22 +0900

    add token

--- src/components/RightComponents/UserProfile.tsx
+++ src/components/RightComponents/UserProfile.tsx
@@ -6,6 +6,7 @@ const UserProfile: React.FC = () => {
 
   if (loading) return <div>Loading...</div>
   if (!user) return <div>未ログイン</div>;
+  const token = localStorage.getItem('token');
 
   return (
     <>
@@ -13,7 +14,7 @@ const UserProfile: React.FC = () => {
       ID：{user.id}<br />
       名前：{user.name}<br />
       email：{user.email}<br />
-      JWT token：{}<br />
+      JWT token：{token}<br />
     </>
   );
 };
